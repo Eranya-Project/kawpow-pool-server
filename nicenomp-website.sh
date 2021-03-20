09ba3604853e67483e3465318760917f716c7777
aymoon05 <79243339+aymoon05@users.noreply.github.com>
2021-03-21 00:55:13 +0200

    Update nicenomp-website.sh

--- nicenomp-website.sh
+++ nicenomp-website.sh
@@ -3,8 +3,8 @@ git clone https://github.com/aymoon05/NiceNOMP.git nn
 cp nn/website ./ -r
 rm libs/api.js libs/stats.js libs/website.js
 cp nn/libs/api.js libs/
-cp nn/libs/stats.js libs/
-cp nn/libs/website.js libs/
+cp libs/stats.js.n libs/stats.js
+cp libs/website.js.n libs/website.js
 rm -rf nn
 rm website/index.html
 cp i.nn website/index.html
